2ac84b00be2a184d67ca039ac0d6eb11c4937309
Mateusz Błaszczyk <mateusz.blaszczyk@fitqbe.com>
2019-04-18 14:09:46 +0200

    1 wersja

--- przywitanie.py
+++ przywitanie.py
@@ -1,3 +1,17 @@
 imie = input("Jak masz na imie:")
 
-print("Witaj "+imie+" w moim programie!")
+print("Witaj "+imie+" w moim programie!
+
+print("Sprawdz ile dwojek miesci sie w podanej przez ciebie liczbie!")
+
+
+liczba = int(input("Podaj liczbe: "))
+
+ilosc_dwojek = 0
+
+
+while liczba >= 2:
+    liczba = liczba - 2
+    ilosc_dwojek +=1
+
+print(ilosc_dwojek)
